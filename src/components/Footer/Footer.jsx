0f1c6fa249dd7d36f4cccdc897a3e315eccd15ec
Navneet Kumar <navneetkumar.html@gmail.com>
2024-04-30 19:13:55 +0530

    testing1

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <>
+    <footer className='w-full min-h-10 bg-slate-800 text-white relative'>
+    <div className='absolute bg-blue-950 inset-0 flex justify-center items-center'>
+        <h1 className="text-center">
+            made with ❤ by <a href='https://navneet.website/'>Navneet</a>
+        </h1>
+    </div>
+</footer>
+
+    </>
+  );
+}
+
+export default Footer;
